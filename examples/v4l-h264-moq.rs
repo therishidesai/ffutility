f77e8336bcb8ca5d03b022328e7bce1cbcf9b16e
Rishi Desai <rishi@saronic.com>
2024-11-16 12:53:15 -0600

    cleaned up nal parsing

--- examples/v4l-h264-moq.rs
+++ examples/v4l-h264-moq.rs
@@ -17,6 +17,8 @@ use std::sync::mpsc::channel;
 
 use tokio::io::{AsyncRead, AsyncReadExt};
 
+use tracing_subscriber::EnvFilter;
+
 use url::Url;
 
 struct AnnexBImport {
@@ -104,30 +106,23 @@ impl AnnexBImport {
             bail!("AnnexBImport not initialized");
         }
 
+        let now = std::time::Instant::now();
+
         let ctx = self.ctx.as_ref().unwrap();
         // NOTE: Have to use a sync channel since I want to mutate
         // both in the AnnexBReader accumulate closure and out of the
         // closure. There is probably a better way of doing this. The
         // h264_reader library has some weird API choices so it may
         // also be worth writing our own h264 NAL parser.
-        let (full_nal_tx, full_nal_rx) = channel();
-        let is_keyframe = Cell::new(false);
-        let found_nal = Cell::new(false);
-        let mut found_sps = Cell::new(false);
-        let mut found_pps = Cell::new(false);
+        let (frame_tx, frame_rx) = channel();
+
+        let mut sps = Cell::new(BytesMut::new());
+        let mut pps = Cell::new(BytesMut::new());
 
         let mut reader = AnnexBReader::accumulate(|nal: RefNal<'_>| {
             let nal_unit_type = nal.header().unwrap().nal_unit_type();
             match nal_unit_type {
-                UnitType::SeqParameterSet | UnitType::PicParameterSet => {
-                    if nal_unit_type == UnitType::SeqParameterSet {
-                        found_sps.set(true);
-                    }
-
-                    if nal_unit_type == UnitType::PicParameterSet {
-                        found_pps.set(true);
-                    }
-                    eprintln!("got sps/pps");
+                UnitType::PicParameterSet => {
                     if nal.is_complete() {
                         let mut nal_reader = nal.reader();
                         let mut full_nal_buf = BytesMut::new();
@@ -140,18 +135,38 @@ impl AnnexBImport {
                                 full_nal_buf.extend_from_slice(&buf);
                             }
                         }
+                        eprintln!("got pps, {}", full_nal_buf.is_empty());
+                        pps.set(full_nal_buf);
+                    }
+                    NalInterest::Buffer
+                }
+                UnitType::SeqParameterSet => {
+                    if nal.is_complete() {
 
-                        full_nal_tx.send(full_nal_buf).unwrap();
+                        let mut nal_reader = nal.reader();
+                        let mut full_nal_buf = BytesMut::new();
+                        let mut buf = [0u8; 1024];
+                        loop {
+                            let n = nal_reader.read(&mut buf).unwrap();
+                            if n == 0 {
+                                break
+                            } else {
+                                full_nal_buf.extend_from_slice(&buf);
+                            }
+                        }
+
+                        eprintln!("got sps, {}", full_nal_buf.is_empty());
+                        sps.set(full_nal_buf);
                     }
                     NalInterest::Buffer
                 },
                 UnitType::SliceLayerWithoutPartitioningNonIdr => {
                     if nal.is_complete() {
-                        found_nal.set(true);
+                        let ts = now.elapsed().as_micros();
+
                         let slice_header = SliceHeader::from_bits(&ctx, &mut nal.rbsp_bits(), nal.header().unwrap()).unwrap().0;
-                        if slice_header.slice_type.family == SliceFamily::I {
-                            is_keyframe.set(true);
-                        }
+                        eprintln!("slice type: {:?}", slice_header.slice_type.family);
+                        let keyframe = slice_header.slice_type.family == SliceFamily::I;
 
                         let mut nal_reader = nal.reader();
                         let mut full_nal_buf = BytesMut::new();
@@ -165,15 +180,35 @@ impl AnnexBImport {
                             }
                         }
 
-                        full_nal_tx.send(full_nal_buf).unwrap();
+                        if !keyframe {
+                            let frame = Frame {
+                                timestamp: Timestamp::from_micros(ts as u64),
+                                keyframe,
+                                payload: full_nal_buf.freeze(),
+                            };
+                            frame_tx.send(frame).unwrap();
+                        } else if keyframe && !pps.get_mut().is_empty() && !sps.get_mut().is_empty() {
+                            let sps = sps.get_mut();
+                            let pps = pps.get_mut();
+                            let mut payload = BytesMut::new();
+                            payload.extend_from_slice(&sps);
+                            payload.extend_from_slice(&pps);
+                            payload.extend_from_slice(&full_nal_buf);
+                            let frame = Frame {
+                                timestamp: Timestamp::from_micros(ts as u64),
+                                keyframe,
+                                payload: payload.freeze(),
+                            };
+                            frame_tx.send(frame).unwrap();
+                        }
                     }
                     NalInterest::Buffer
                 },
                 UnitType::SliceLayerWithoutPartitioningIdr => {
-                    if nal.is_complete() {
-                        found_nal.set(true);
-                        is_keyframe.set(true);
-
+                    eprintln!("got IDR");
+                    if nal.is_complete() && !pps.get_mut().is_empty() && !sps.get_mut().is_empty() {
+                        eprintln!("FULL NAL and SPS and PPS");
+                        let ts = now.elapsed().as_micros();
                         let mut nal_reader = nal.reader();
                         let mut full_nal_buf = BytesMut::new();
                         let mut buf = [0u8; 1024];
@@ -185,9 +220,20 @@ impl AnnexBImport {
                                 full_nal_buf.extend_from_slice(&buf);
                             }
                         }
-                        full_nal_tx.send(full_nal_buf).unwrap();
-                    }
 
+                        let sps = sps.get_mut();
+                        let pps = pps.get_mut();
+                        let mut payload = BytesMut::new();
+                        payload.extend_from_slice(&sps);
+                        payload.extend_from_slice(&pps);
+                        payload.extend_from_slice(&full_nal_buf);
+                        let frame = Frame {
+                            timestamp: Timestamp::from_micros(ts as u64),
+                            keyframe: true,
+                            payload: payload.freeze(),
+                        };
+                        frame_tx.send(frame).unwrap();
+                    }
                     NalInterest::Buffer
                 },
                 _ => {
@@ -198,46 +244,17 @@ impl AnnexBImport {
         });
 
         let mut buffer = BytesMut::new();
-        let now = std::time::Instant::now();
+
         
         
         while input.read_buf(&mut buffer).await? > 0 {
             reader.push(&buffer);
-
-            if found_nal.get() && found_sps.get() && found_pps.get() {
-                println!("Got a full NAL, is_keyframe: {}", is_keyframe.get());
-
-                let ts = now.elapsed().as_micros();
-
-                let mut nal_payload = BytesMut::new();
-
-                loop {
-                    match full_nal_rx.try_recv() {
-                        Ok(nal) => {
-                            nal_payload.extend_from_slice(&nal);
-                        }
-                        Err(std::sync::mpsc::TryRecvError::Empty) => {
-                            break
-                        }
-                        Err(_) => {
-                            panic!("mpsc crashed");
-                        }
-                    }
+            loop {
+                match frame_rx.try_recv() {
+                    Ok(f) => track.write(f),
+                    Err(std::sync::mpsc::TryRecvError::Empty) => break,
+                    Err(_) => panic!("frame_rx channel disconnected"),
                 }
-
-
-                // println!("NAL Payload: {:?}", nal_payload);
-
-                let frame = Frame {
-                    timestamp: Timestamp::from_micros(ts as u64),
-                    keyframe: is_keyframe.get(),
-                    payload: nal_payload.freeze(),
-                };
-                track.write(frame);
-                found_nal.set(false);
-                found_sps.set(false);
-                found_pps.set(false);
-                is_keyframe.set(false);
             }
         }
 
@@ -247,6 +264,10 @@ impl AnnexBImport {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .init();
     let tls = moq_native::tls::Args::default().load()?;
     let quic_client = quic::Endpoint::new(quic::Config {
         bind: SocketAddr::from(([0, 0, 0, 0], 0)),
