7f712e5628858e3dfcb8980a9e28d9daa58c8714
Rishi Desai <rishi@saronic.com>
2025-08-25 17:50:31 -0500

    fix: add back v4l feature flag so you don't always need it (#15)

--- examples/v4l-h264-moq.rs
+++ examples/v4l-h264-moq.rs
@@ -1,11 +1,11 @@
-#![cfg(target_os = "linux")]
+#![cfg(all(target_os = "linux", feature = "v4l"))]
 
 use anyhow::Result;
 
 use ffutility::{encoders::FfmpegOptions, parsers::AnnexBStreamImport, streams::{V4lH264Stream, V4lH264Config}};
 
 use hang::BroadcastProducer;
-use moq_native::quic;
+use moq_native::client;
 
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
@@ -20,21 +20,22 @@ async fn main() -> Result<()> {
         .with_env_filter(EnvFilter::from_default_env())
         .with_writer(std::io::stderr)
         .init();
-    let mut tls_args = moq_native::tls::Args::default();
-    tls_args.disable_verify = true;
-    let tls = tls_args.load()?;
-    let quic_client = quic::Endpoint::new(quic::Config {
+
+    let mut tls = moq_native::client::ClientTls::default();
+    tls.disable_verify = Some(true);
+
+    let quic_client = client::Client::new(client::ClientConfig {
         bind: SocketAddr::from(([0, 0, 0, 0], 0)),
-        tls
+        tls    
     })?;
 
-    let session = quic_client.client.connect(Url::parse("https://relay.quic.video").unwrap()).await?;
+    let session = quic_client.connect(Url::parse("https://relay.quic.video").unwrap()).await?;
 
-    let session = moq_lite::Session::connect(session.into()).await?;
+    let mut session = moq_lite::Session::connect(session).await?;
 
-    let path = moq_lite::Path::new().push("test-zed");
+    let broadcast = BroadcastProducer::new();
 
-    let broadcast = BroadcastProducer::new(session.clone(), path)?;
+    session.publish("test-zed", broadcast.inner.consume());
 
     let mut annexb_import = AnnexBStreamImport::new(Arc::new(Mutex::new(broadcast)), 736, 414);
 
